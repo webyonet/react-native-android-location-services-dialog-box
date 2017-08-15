48c0795153a0c306366b920046ff69c092d25b3d
Serge Seredenko <serge.seredenko@gmail.com>
2017-08-15 04:39:39 +0300

    Simple iOS dummy
    On iOS return value as if GPS was enabled.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-import { NativeModules } from 'react-native';
-export default NativeModules.LocationServicesDialogBox;
+import { NativeModules, Platform } from 'react-native';
+const LocationServicesDialogBox = Platform.OS === 'android' ?
+    NativeModules.LocationServicesDialogBox :
+    {
+        checkLocationServicesIsEnabled: () => Promise.resolve({
+            alreadyEnabled: true,
+            enabled: true,
+            status: "enabled"
+        }),
+        forceCloseDialog: () => {}
+    }
+export default LocationServicesDialogBox;
